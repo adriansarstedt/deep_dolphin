ef53e48ab09bab3ecc294de326f7f0a5d8d09c70
Adrian Sarstedt <adrian.sarstedt@gmail.com>
2020-06-02 17:23:22 +1000

    Added basic exporter scaffold
    Accepts mask slice and returns contour.
    
    Still need to do the following:
     - account for the fact that there may be more than one contour in any
       given slice
     - calculate the contour(s) for each slice within a .nii file
     - wrap in the RTStruct header (thereby allowing the contour to be
       viewed and edited in DICOM viewers)

---
+++ deep_dolphin/exporters/dicom_to_mask_exporter.rb
@@ -0,0 +1,2 @@
+# will be used to convert radiologist segmentations in to a form that
+# can be fed back into the training process
---
+++ spec/exporters/mask_to_dicom_exporter_test.py
@@ -0,0 +1,33 @@
+import unittest
+from deep_dolphin.exporters.mask_to_dicom_exporter import MaskToDicomExporter
+import nibabel as nib
+import matplotlib.pyplot as plt
+import numpy as np
+from deep_dolphin.contouring.contour_builder import ContourBuilder
+
+class MaskToDicomExporterTest(unittest.TestCase):
+
+    def test_(self):
+        example_mask_path = "./spec/fixtures/output_mask.nii.gz"
+        mask = nib.load(example_mask_path)
+
+        (X, Y, Z) = mask.shape
+        mask_data = mask.get_fdata()
+        mask_slice = mask_data[80]
+
+        contour = MaskToDicomExporter().get_contour_for_mask_slice(mask_slice, 10)
+        xs, ys = np.array(contour)[:,1], np.array(contour)[:,0]
+
+        fig = plt.figure(figsize=(4,6))
+        axes = fig.add_axes([0.15,0.1,0.7,0.8])
+        axes.set_xlim([0,Z])
+        axes.set_ylim([0,Y])
+
+        axes.imshow(mask_slice)
+        axes.plot(xs,ys)
+        plt.show()
+
+
+
+if __name__ == '__main__':
+    unittest.main()
