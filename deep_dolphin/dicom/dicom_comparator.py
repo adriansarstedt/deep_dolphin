453394fa8021ae0b703ec99b70dd853058cbec98
Adrian Sarstedt <adrian.sarstedt@gmail.com>
2020-06-18 11:37:15 +1000

    Introduced DicomComparitor
    significantly improves readability of RTStructGenerator test.

--- deep_dolphin/dicom/dicom_comparator.py
+++ deep_dolphin/dicom/dicom_comparator.py
@@ -1,8 +1,50 @@
 import os
 from pydicom import dcmread
+import difflib
 
 class DicomComparator(object):
+    id_tags = [
+        'Media Storage SOP Instance UID',
+        'Series Instance UID',
+        'SOP Instance UID',
+        'Instance Creation Time',
+        'Series Time',
+        'Structure Set Time',
+        'Instance Creation Date',
+        'Series Date',
+        'Structure Set Date'
+    ]
+
     def __init__(self, dcm_1, dcm_2):
         self.dcm_1 = dcm_1
         self.dcm_2 = dcm_2
-        
+
+    def load_content_differences(self):
+        return (
+            list(filter(self.is_content_difference, self.load_all_differences()))
+        )
+
+    def load_all_differences(self):
+        return ( 
+            list(filter(self.is_marked_as_difference, self.get_comparison()))
+        )
+
+    def get_comparison(self):
+        return ( difflib.Differ().compare(
+                    str(self.dcm_1).split("\n"), 
+                    str(self.dcm_2).split("\n")) ) 
+
+    def is_content_difference(self, difference):
+        return (
+            not self.is_id_difference(difference)
+        )
+
+    def is_id_difference(self, difference):
+        for tag in self.id_tags:
+            if tag in difference:
+                return True
+
+    def is_marked_as_difference(self, line):
+        return (
+            (line[0] == '+') or (line[0] == '-')
+        )
--- tests/dicom/dicom_comparator_test.py
+++ tests/dicom/dicom_comparator_test.py
@@ -7,9 +7,41 @@ class DicomComparatorTest(unittest.TestCase):
     maxDiff = None
 
     def setUp(self):
-        return(None)
-        #dcm_1 = dcmread()
-        #dcm_2 = dcmread()
+        self.dcm_1 = dcmread('./fixtures/dicom/compressed_study/rtstruct.dcm')
+        self.dcm_2 = dcmread('./fixtures/dicom/compressed_study/rtstruct.dcm')
+
+    def test_identical_dicoms(self):
+        comparator = DicomComparator(self.dcm_1, self.dcm_2)
+
+        self.assertEquals(comparator.load_all_differences(), [])
+        self.assertEquals(comparator.load_content_differences(), [])
+
+    def test_unidentical_dicoms(self):
+
+        # id differences
+        self.dcm_1.MediaStorageSOPInstanceUID = '1'
+        self.dcm_2.MediaStorageSOPInstanceUID = '2'
+        self.dcm_1.SeriesDate = '11111111'
+        self.dcm_2.SeriesDate = '22222222'
+
+        # content differences
+        self.dcm_1.ReferringPhysicianName = 'Bob^Stokes'
+        self.dcm_1.ReferringPhysicianName = 'Bob^Dylan'
+
+        comparator = DicomComparator(self.dcm_1, self.dcm_2)
+
+        self.assertEquals(comparator.load_all_differences(), [        
+            '- (0002, 0003) Media Storage SOP Instance UID      UI: 1',
+            '+ (0002, 0003) Media Storage SOP Instance UID      UI: 2',
+            '- (0008, 0021) Series Date                         DA: \'11111111\'',
+            '+ (0008, 0021) Series Date                         DA: \'22222222\'',
+            '- (0008, 0090) Referring Physician\'s Name          PN: \'Bob^Dylan\'',
+            '+ (0008, 0090) Referring Physician\'s Name          PN: \'WADA^MORIKATSU\''])
+        self.assertEquals(comparator.load_content_differences(), [
+            '- (0008, 0090) Referring Physician\'s Name          PN: \'Bob^Dylan\'',
+            '+ (0008, 0090) Referring Physician\'s Name          PN: \'WADA^MORIKATSU\''])
+
+
 
 if __name__ == '__main__':
     unittest.main()
