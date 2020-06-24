a403a55c75eef3977dbf2ed671de44e98a069ca2
Adrian Sarstedt <adrian.sarstedt@gmail.com>
2020-06-24 13:33:12 +1000

    Cleaned EdgeDetector

--- deep_dolphin/contouring/edge_detector.py
+++ deep_dolphin/contouring/edge_detector.py
@@ -1,27 +1,29 @@
 import numpy as np
 
-class EdgeDetector(object):
 
+class EdgeDetector(object):
     def __init__(self, slice):
-        self.slice = slice
+        self.slice = np.array(slice)
+        self.width, self.height = self.slice.shape
 
     def get_edge_points(self):
         edge_points = []
-        for x in range(1, len(self.slice)-1):
-            for y in range(1, len(self.slice[0])-1):
-                if self.is_non_zero(x, y, self.slice) and self.is_edge_point(x, y, self.slice):
+        for x in range(1, self.width - 1):
+            for y in range(1, self.height - 1):
+                if self.is_edge_point(x, y):
                     edge_points.append((x, y))
-        return( edge_points )
+        return edge_points
 
-    def is_edge_point(self, x, y, mask):
+    def is_edge_point(self, x, y):
+        return self.is_not_empty_pixel(x, y) and self.is_neighbouring_empty_pixel(x, y)
+
+    def is_neighbouring_empty_pixel(self, x, y):
         return (
-            (mask[x+1][y] < 0.5) or
-            (mask[x-1][y] < 0.5) or
-            (mask[x][y+1] < 0.5) or
-            (mask[x][y-1] < 0.5)
+            (self.slice[x + 1][y] < 0.5)
+            or (self.slice[x - 1][y] < 0.5)
+            or (self.slice[x][y + 1] < 0.5)
+            or (self.slice[x][y - 1] < 0.5)
         )
 
-    def is_non_zero(self, x, y, mask):
-        return (
-            mask[x][y] > 0.5
-        )
+    def is_not_empty_pixel(self, x, y):
+        return self.slice[x][y] > 0.5
